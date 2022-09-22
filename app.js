1830d1a9233e668579af3820b19d6522e43e8931
rshashank11 <shashankyadav2000@gmail.com>
2022-09-22 20:52:33 +0530

    Added more functionalities.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const taskSections = [...document.querySelectorAll(".task-section")];
 const taskTitles = [...document.querySelectorAll(".task-title")];
 const taskDescs = [...document.querySelectorAll(".task-description")];
-
+const dragContainers = [...document.querySelectorAll(".drag-container")]
 
 const addCards = [...document.querySelectorAll(".add-card")];
 const addTaskContainers = [...document.querySelectorAll(".add-task-container")];
@@ -29,7 +29,8 @@ saveBtns.forEach((saveBtn) => {
             addTaskContainers[saveBtnIndex].style.display = "none";
         } else {
             const taskContainer = document.createElement("div");
-            taskContainer.className = "task-container";
+            taskContainer.classList.add("task-container");
+            taskContainer.classList.add("draggable");
 
             const taskTitleEl = document.createElement("p");
             taskTitleEl.innerText = taskTitles[saveBtnIndex].value;
@@ -40,13 +41,58 @@ saveBtns.forEach((saveBtn) => {
             taskDescEl.innerText = taskDescs[saveBtnIndex].value;
             taskDescEl.className = "task-description-p";
             taskContainer.appendChild(taskDescEl);
+            taskContainer.setAttribute("draggable", true);
 
             taskTitles[saveBtnIndex].value = null;
             taskDescs[saveBtnIndex].value = null;
 
-            taskSections[saveBtnIndex].appendChild(taskContainer);
+            dragContainers[saveBtnIndex].appendChild(taskContainer);
             addCards[saveBtnIndex].style.display = "block";
             addTaskContainers[saveBtnIndex].style.display = "none";
         }
+
+        const draggables = document.querySelectorAll(".draggable");
+        draggables.forEach((draggable) => {
+            draggable.addEventListener("dragstart", () => {
+                draggable.classList.add("dragging");
+            });
+
+            draggable.addEventListener("dragend", () => {
+                draggable.classList.remove("dragging");
+            })
+        });
     })
 })
+
+
+dragContainers.forEach((dragContainer) => {
+    dragContainer.addEventListener("dragover", (event) => {
+        event.preventDefault();
+        const draggable = document.querySelector(".dragging");
+        const afterEl = getDragUponEl(dragContainer, event.clientY);
+        if(afterEl == null) {
+            dragContainer.appendChild(draggable);
+        } else {
+            dragContainer.insertBefore(draggable, afterEl);
+        }
+    })
+})
+
+
+function getDragUponEl(dragContainer, y) {
+    const dragContainers = [...dragContainer.querySelectorAll('.draggable:not(dragging)')];
+    return dragContainers.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - (box.height / 2);
+        if (offset < 0 && offset > closest.offset) {
+            return {
+                offset: offset,
+                element: child
+            }
+        } else {
+            return closest;
+        }
+    }, {
+        offset: Number.NEGATIVE_INFINITY
+    }).element;
+}
