d68fcb024f1dc2c689c1041ea1bc27c14575a746
rshashank11 <shashankyadav2000@gmail.com>
2022-09-24 19:32:29 +0530

    Added local storage

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const addCards = [...document.querySelectorAll(".add-card")];
 const addTaskContainers = [...document.querySelectorAll(".add-task-container")];
 const saveBtns = [...document.querySelectorAll(".save-button")];
 
+document.addEventListener("DOMContentLoaded", getTasks);
+
 heading.addEventListener("dblclick", () => {
     heading.toggleAttribute("readonly");
 })
@@ -59,6 +61,8 @@ saveBtns.forEach((saveBtn) => {
             dragContainers[saveBtnIndex].appendChild(taskContainer);
             addCards[saveBtnIndex].style.display = "block";
             addTaskContainers[saveBtnIndex].style.display = "none";
+
+            saveTasks(taskTitleEl, taskDescEl, saveBtnIndex);
         }
 
         const draggables = document.querySelectorAll(".draggable");
@@ -80,7 +84,7 @@ dragContainers.forEach((dragContainer) => {
         event.preventDefault();
         const draggable = document.querySelector(".dragging");
         const afterEl = getDragUponEl(dragContainer, event.clientY);
-        if(afterEl == null) {
+        if (afterEl == null) {
             dragContainer.appendChild(draggable);
         } else {
             dragContainer.insertBefore(draggable, afterEl);
@@ -105,4 +109,77 @@ function getDragUponEl(dragContainer, y) {
     }, {
         offset: Number.NEGATIVE_INFINITY
     }).element;
+}
+
+function saveTasks(task, desc, containerIndex) {
+    console.log(task.innerText)
+    console.log(desc.innerText)
+    console.log(containerIndex)
+    let tasksTitles, tasksDescs;
+    let containers;
+
+    if (localStorage.getItem("tasksTitles") === null) {
+        tasksTitles = [];
+    } else {
+        tasksTitles = JSON.parse(localStorage.getItem("tasksTitles"));
+    }
+
+    if (localStorage.getItem("tasksDescs") === null) {
+        tasksDescs = [];
+    } else {
+        tasksDescs = JSON.parse(localStorage.getItem("tasksDescs"));
+    }
+
+    if (localStorage.getItem("containers") === null) {
+        containers = [];
+    } else {
+        containers = JSON.parse(localStorage.getItem("containers"));
+    }
+    
+    tasksTitles.push(task.innerText);
+    localStorage.setItem("tasksTitles", JSON.stringify(tasksTitles));
+    tasksDescs.push(desc.innerText);
+    localStorage.setItem("tasksDescs", JSON.stringify(tasksDescs));
+    containers.push(containerIndex);
+    localStorage.setItem("containers", JSON.stringify(containers));
+}
+
+function getTasks() {
+    let tasksTitles, tasksDescs, containers;
+    if (localStorage.getItem("tasksTitles") === null) {
+        tasksTitles = []
+    } else {
+        tasksTitles = JSON.parse(localStorage.getItem("tasksTitles"));
+    }
+
+    if (localStorage.getItem("tasksDescs") === null) {
+        tasksDescs = [];
+    } else {
+        tasksDescs = JSON.parse(localStorage.getItem("tasksDescs"));
+    }
+
+    if (localStorage.getItem("containers") === null) {
+        containers = [];
+    } else {
+        containers = JSON.parse(localStorage.getItem("containers"));
+    }
+
+    tasksTitles.forEach((tasksTitle, index) => {
+        const taskContainer = document.createElement("div");
+        taskContainer.classList.add("task-container");
+        taskContainer.classList.add("draggable");
+
+        const taskTitleEl = document.createElement("p");
+        taskTitleEl.innerText = tasksTitle;
+        taskTitleEl.className = "task-title-p";
+        taskContainer.appendChild(taskTitleEl);
+
+        const taskDescEl = document.createElement("p");
+        taskDescEl.innerText = tasksDescs[index];
+        taskDescEl.className = "task-description-p";
+        taskContainer.appendChild(taskDescEl);
+
+        taskContainer.setAttribute("draggable", true);
+        dragContainers[containers[index]].appendChild(taskContainer);
+    })
 }
